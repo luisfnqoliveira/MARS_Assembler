cfd35e2b4e39f8fc3b93d8585a20e96f9bb169a9
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2023-08-15 13:52:58 -0400

    FIXED: labels window highlight no longer makes text invisible

--- mars/venus/LabelsWindow.java
+++ mars/venus/LabelsWindow.java
@@ -214,7 +214,6 @@ public class LabelsWindow extends JPanel
 				// versus the perceived effort to make reordering work for multiple symbol tables,
 				// I am taking the easy way out here.  PS 19 July 2007.
 				tableHeader.setReorderingAllowed(false);
-				table.setSelectionBackground(table.getBackground());
 				// Sense click on label/address and scroll Text/Data segment display to it.
 				table.addMouseListener(new LabelDisplayMouseListener());
 				allSymtabTables.add(table);
