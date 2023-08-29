605799ee0a9c005baa6be2f7408c2ba3c40a09b1
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2023-08-28 20:47:51 -0400

    windows package!

---
+++ build_exe.bat
@@ -0,0 +1,11 @@
+jpackage ^
+	--input .\jar\ ^
+	--dest .\downloads\ ^
+	--name Mars_2241_0828 ^
+	--icon .\images\mars.ico ^
+	--main-jar Mars_2241_0828.jar ^
+	--type exe ^
+	--app-version "4.5.41828" ^
+	--vendor "com.jfbillingsley" ^
+	--win-menu ^
+	--verbose
