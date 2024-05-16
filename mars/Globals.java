1ecf773e45a00825694323164b5821c19e9a2393
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2024-05-15 22:43:44 -0400

    version string, jar, and mac package

--- mars/Globals.java
+++ mars/Globals.java
@@ -79,7 +79,7 @@ public class Globals
 	/* The GUI being used (if any) with this simulator. */
 	static VenusUI gui = null;
 	/** The current MARS version number. Can't wait for "initialize()" call to get it. */
-	public static final String version = "4.5 (2244_0130)";
+	public static final String version = "4.5 (2247_0515)";
 	/** List of accepted file extensions for MIPS assembly source files. */
 	public static final ArrayList fileExtensions = getFileExtensions();
 	/** Maximum length of scrolled message window (MARS Messages and Run I/O) */
